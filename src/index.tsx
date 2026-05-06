878052f6721f07ea333cc6d1bd9cd54cab8bdfc9
Colby Gilbert <colby@firmware.ai>
2026-05-06 12:33:50 -0700

    fix: proper .js exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,2 @@
-export { default } from './components/ReactHlsPlayer'
-export type { ReactHlsPlayerProps } from './components/ReactHlsPlayer'
+export { default } from './components/ReactHlsPlayer.js'
+export type { ReactHlsPlayerProps } from './components/ReactHlsPlayer.js'
